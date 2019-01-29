9c9362b41d3c330074a9f980aadf6bb7b27918d1
paulmarrington <paul@marrington.net>
2019-01-29 17:32:08 +1000

    29/01/2019

---
+++ Assets/Askowl/Adze/Examples/AdzeServiceGherkinDefinitions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2019 (C) paul@marrington.net http://www.askowl.net/unity-packages
+
+using System;
+using Askowl.Gherkin;
+using NUnit.Framework;
+// ReSharper disable MissingXmlDoc
+
+namespace Tests {
+  [Serializable] public class AdzeServiceGherkinDefinitions : GherkinDefinitions {
+    [Test] public void AdzeService() {
+      var feature = Definitions.Load("AdzeServiceFeature");
+      if (!feature.Run("AdzeService")) Assert.Fail(feature.output);
+    }
+
+    [Step(@"\d+ services available")] public void ServicesAvailable() { }
+  }
+}
